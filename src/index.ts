8fc344b4e0fff1f0cbf7554432932a7b939e2690
Ced <jcedb07@gmail.com>
2022-03-11 13:44:02 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+type TTarget = string | HTMLElement | Element | any;
+
+const getElement = (target: TTarget) => {
+  const isString = typeof target === 'string';
+
+  return isString ? document.querySelector(target) : target;
+};
+
+export const sticky = (target: TTarget) => {
+  const targetOffset = getElement(target).offsetHeight;
+
+  const body = document.querySelector('body') as HTMLBodyElement;
+  const style = getElement(target).style;
+
+  window.onscroll = () => {
+    if (window.scrollY > targetOffset) {
+      style.position = 'fixed';
+      style.top = '0';
+      style.width = '100%';
+      body.style.paddingTop = `${getElement(target).offsetHeight}px`;
+    } else {
+      style.position = '';
+      style.top = '';
+      style.width = '';
+      body.style.paddingTop = '';
+    }
+  };
+};
